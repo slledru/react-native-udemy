02bf79c4b681dae95e9b913b9b6870e6778339fb
slledru <slledru@msn.com>
2018-06-23 14:23:09 -0600

    first react native app example

---
+++ albums/src/components/AlbumDetail.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import { Image, Text, View, Linking } from 'react-native'
+import Card from './Card'
+import CardSection from './CardSection'
+import Button from './Button'
+
+const AlbumDetail = ({ album }) => {
+  const { title, artist, thumbnail_image, image, url } = album
+  const {
+    thumbnailStyle,
+    textContainer,
+    textStyle,
+    thumbnailContainerStyle,
+    imageStyle
+  } = styles
+  return (
+    <Card>
+      <CardSection>
+        <View style={ thumbnailContainerStyle }>
+          <Image
+            style={ thumbnailStyle }
+            source={{ uri: thumbnail_image }}
+          />
+        </View>
+        <View style={ textContainer }>
+          <Text style={ textStyle }>{ title }</Text>
+          <Text>{ artist }</Text>
+        </View>
+      </CardSection>
+      <CardSection>
+        <Image
+          source={{ uri: image }}
+          style={ imageStyle }/>
+      </CardSection>
+      <CardSection>
+        <Button onPress={() => Linking.openURL(url) }>
+          Buy the Album Now
+        </Button>
+      </CardSection>
+    </Card>
+  )
+}
+
+const styles = {
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
+  },
+  textContainer: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  textStyle: {
+    fontSize: 18
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  }
+}
+export default AlbumDetail
---
+++ albums/src/components/AlbumList.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import { ScrollView } from 'react-native'
+import axios from 'axios'
+import AlbumDetail from './AlbumDetail'
+
+class AlbumList extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { albums: [] }
+  }
+
+  componentWillMount() {
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then((response) => this.setState({ ...this.state, albums: response.data }))
+  }
+
+  renderAlbums() {
+    return this.state.albums.map((album) =>
+      <AlbumDetail key={ album.url } album={ album } />
+    )
+  }
+
+  render() {
+    console.log(this.state)
+    return (
+      <ScrollView>
+        { this.renderAlbums() }
+      </ScrollView>
+    )
+  }
+}
+
+const styles = {
+  scrollViewStyle: {
+    flex: 1
+  }
+}
+
+export default AlbumList
