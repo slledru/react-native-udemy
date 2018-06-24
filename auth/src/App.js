0ed7899b5514955bf2fdd17b10477d33a5c87134
slledru <slledru@msn.com>
2018-06-23 18:21:10 -0600

    read from .env

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react'
 import { View, Text } from 'react-native'
+import firebase from 'firebase'
+import { FIREBASE_API_KEY, FIREBASE_AUTH_DOMAIN,
+  FIREBASE_DB_URL, FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET, FIREBASE_MSG_SENDER_ID
+} from 'react-native-dotenv'
 import { Header } from './components/common'
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: FIREBASE_API_KEY,
+      authDomain: FIREBASE_AUTH_DOMAIN,
+      databaseURL: FIREBASE_DB_URL,
+      projectId: FIREBASE_PROJECT_ID,
+      storageBucket: FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: FIREBASE_MSG_SENDER_ID
+    })
+  }
+
   render() {
     return (
       <View style={{ flex: 1 }}>
