15f66f681cfd9ed93191925e5ebb384ec8c03744
slledru <slledru@msn.com>
2018-06-24 18:18:39 -0600

    added firebase

--- manager/src/App.js
+++ manager/src/App.js
@@ -1,19 +1,44 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { Component } from 'react'
+import { View } from 'react-native'
+import firebase from 'firebase'
+
+import { FIREBASE_API_KEY, FIREBASE_AUTH_DOMAIN,
+  FIREBASE_DB_URL, FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET, FIREBASE_MSG_SENDER_ID
+} from 'react-native-dotenv'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 
 import rootReducers from './reducers'
 import { Header } from './components/common'
 
-const App = () => {
-  return (
-    <Provider store={ createStore(rootReducers) }>
-      <View style={ styles.container }>
-        <Header headerText={ 'Manager' }/>
-      </View>
-    </Provider>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+  }
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: FIREBASE_API_KEY,
+      authDomain: FIREBASE_AUTH_DOMAIN,
+      databaseURL: FIREBASE_DB_URL,
+      projectId: FIREBASE_PROJECT_ID,
+      storageBucket: FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: FIREBASE_MSG_SENDER_ID
+    })
+    firebase.auth().onAuthStateChanged((user) => {
+      console.log('user', user)
+    })
+  }
+
+  render() {
+    return (
+      <Provider store={ createStore(rootReducers) }>
+        <View style={ styles.container }>
+          <Header headerText={ 'Manager' }/>
+        </View>
+      </Provider>
+    )
+  }
 }
 
 const styles = {
