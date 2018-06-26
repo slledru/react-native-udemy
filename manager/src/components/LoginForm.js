50586a6403a469269ded454083cf740c54b3c08e
slledru <slledru@msn.com>
2018-06-25 19:38:41 -0600

    save email and password in redux

--- manager/src/components/LoginForm.js
+++ manager/src/components/LoginForm.js
@@ -1,16 +1,29 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 import { Button, Card, CardSection, TextField } from './common'
+import { emailChanged, passwordChanged } from '../actions'
 
 class LoginForm extends Component {
+  onEmailChange = (text) => {
+    this.props.emailChanged(text)
+  }
+
+  onPasswordChange = (text) => {
+    this.props.passwordChanged(text)
+  }
+
   render() {
     return (
       <Card>
         <CardSection>
           <TextField label="Email"
+            onChangeText={ this.onEmailChange }
             placeholder="user@email.com" />
         </CardSection>
         <CardSection>
           <TextField
+            onChangeText={ this.onPasswordChange }
             secureTextEntry
             label="Password"
             placeholder="password" />
@@ -25,4 +38,8 @@ class LoginForm extends Component {
   }
 }
 
-export default LoginForm
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({ emailChanged, passwordChanged }, dispatch)
+}
+
+export default connect(null, mapDispatchToProps)(LoginForm)
