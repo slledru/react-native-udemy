a707331e870335d8789bf3302f6baac4146c1be1
slledru <slledru@msn.com>
2018-06-26 20:14:10 -0600

    connected create employee form with redux

--- manager/src/components/EmployeeCreate.js
+++ manager/src/components/EmployeeCreate.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 import { Card, CardSection, Button, TextField } from './common'
+import { updateEmployee } from '../actions'
 
 class EmployeeCreate extends Component {
   render() {
     return (
       <Card>
         <CardSection>
-          <TextField 
-            label="Name" placeholder="Jane Doe" />
+          <TextField
+            label="Name" placeholder="Jane Doe"
+            value={ this.props.name }
+            onChangeText={ (text) => this.props.updateEmployee({ prop: 'name', value: text }) }/>
         </CardSection>
         <CardSection>
-          <TextField label="Phone" placeholder="555-555-5555" />
+          <TextField label="Phone" placeholder="555-555-5555"
+            value={ this.props.phone }
+            onChangeText={ (text) => this.props.updateEmployee({ prop: 'phone', value: text }) }/>
         </CardSection>
         <CardSection>
         </CardSection>
@@ -24,4 +31,13 @@ class EmployeeCreate extends Component {
   }
 }
 
-export default EmployeeCreate
+const mapStateToProps = (state) => {
+  const { name, phone, shift } = state.employeeForm
+  return { name, phone, shift }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({ updateEmployee }, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(EmployeeCreate)
